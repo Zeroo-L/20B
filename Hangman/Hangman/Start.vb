8b33934799ba43ff7f5c19ffcaf8edb48c5cad2f
Zeroo-L <legolaslauprivate@gmail.com>
2022-05-15 21:35:57 +1000

    Basic Hangman
    Done:
    -Guessing of the word
    -Basic form navigation
    -Binary Search of the input in the word

---
+++ Hangman/Hangman/Start.vb
@@ -0,0 +1,42 @@
+﻿Public Class Start
+    Dim GuessWord As String = "Apple"
+    Dim WordArray(0) As String
+    Dim ProgressArray(0) As String
+    Dim GuessedLetters(26) As String
+    Dim GuessedIndex As Integer = 0
+
+    Private Sub Start_Button_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        StartPanel.Visible = False
+        GamePanel.Visible = True
+
+        ReDim WordArray(GuessWord.Length() - 1)
+        ReDim ProgressArray(GuessWord.Length() - 1)
+
+        For index = 0 To GuessWord.Length - 1
+            WordArray(index) = GuessWord.Chars(index)
+        Next
+
+        For index = 0 To GuessWord.Length - 1
+            ProgressArray(index) = "_"
+        Next
+
+        For index = 0 To WordArray.Length - 1
+            GuessLetters.Text = Join(ProgressArray, " ")
+        Next
+
+    End Sub
+
+    Private Sub Button_Enter_Click(sender As Object, e As EventArgs) Handles Button_Enter.Click
+        For index = 0 To GuessWord.Length - 1
+            If InputBox.Text = WordArray(index) Then
+                ProgressArray(index) = WordArray(index)
+            End If
+        Next
+
+        InputBox.Clear()
+
+        For index = 0 To WordArray.Length - 1
+            GuessLetters.Text = Join(ProgressArray, " ")
+        Next
+    End Sub
+End Class
