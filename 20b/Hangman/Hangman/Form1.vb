04a223ba11f6a848e47f0ccebaf55ccab7e67ce8
Zeroo-L <101458675+Zeroo-L@users.noreply.github.com>
2022-05-16 10:21:24 +1000

    Add files via upload

---
+++ 20b/Hangman/Hangman/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Sub panelswitch(ByVal panel As Form)
+        Panel1.Controls.Clear()
+        panel.TopLevel = False
+        Panel1.Controls.Add(panel)
+        panel.Show()
+    End Sub
+End Class
---
+++ 20b/Hangman/Hangman/Start.vb
@@ -0,0 +1,62 @@
+﻿Imports System.IO
+
+Public Class Start
+    Dim Path As String = ""
+    Dim FileArray() As String = File.ReadAllLines(Path)
+    Dim GuessWord As String = "Apple"
+    Dim WordArray(0) As String
+    Dim ProgressArray(0) As String
+    Dim GuessedLetters(26) As String
+    Dim GuessedIndex As Integer = 0
+    Dim lives As Integer = 10
+
+    Private Sub Start_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        File.WriteAllLines(Path, FileArray)
+    End Sub
+
+    Private Sub Start_Button_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        StartPanel.Visible = False
+        GamePanel.Visible = True
+
+        ReDim WordArray(GuessWord.Length() - 1)
+        ReDim ProgressArray(GuessWord.Length() - 1)
+
+        For index = 0 To GuessWord.Length - 1
+            WordArray(index) = GuessWord.Chars(index)
+        Next
+
+        For index = 0 To GuessWord.Length - 1
+            ProgressArray(index) = "_"
+        Next
+
+        For index = 0 To WordArray.Length - 1
+            GuessLetters.Text = Join(ProgressArray, " ")
+        Next
+
+    End Sub
+
+    Private Sub Button_Enter_Click(sender As Object, e As EventArgs) Handles Button_Enter.Click
+
+        For index = 0 To 26
+            If InputBox.Text = GuessedLetters(index) Then
+                MsgBox("|" & InputBox.Text & "| has already been used", 0, "Message")
+
+                Exit Sub
+            End If
+        Next
+
+        For index = 0 To GuessWord.Length - 1
+            If InputBox.Text = WordArray(index) Then
+                ProgressArray(index) = WordArray(index)
+            End If
+        Next
+
+
+
+        InputBox.Clear()
+
+        For index = 0 To WordArray.Length - 1
+            GuessLetters.Text = Join(ProgressArray, " ")
+        Next
+    End Sub
+End Class
