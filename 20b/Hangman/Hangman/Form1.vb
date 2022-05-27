205050085be8e23b71bc091abc79ced6184cae4f
Legolas Lau <legolas.lau@DETNSW>
2022-05-27 15:08:23 +1000

    Delete Folder

--- 20b/Hangman/Hangman/Form1.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form1
-    Sub panelswitch(ByVal panel As Form)
-        Panel1.Controls.Clear()
-        panel.TopLevel = False
-        Panel1.Controls.Add(panel)
-        panel.Show()
-    End Sub
-End Class
