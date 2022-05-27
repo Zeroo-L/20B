ba95ae4d9a1d560b1c50b7c9cb2c616d590e8340
Legolas Lau <legolas.lau@DETNSW>
2022-05-27 15:02:14 +1000

    Fixed Loop

--- Hangman/Hangman/Start.vb
+++ Hangman/Hangman/Start.vb
@@ -7,8 +7,10 @@ Public Class Start
     Dim GuessedLetters(25) As String
     Dim GuessedIndex As Integer = 0
     Dim Lives As Integer = 10
+    Dim Correct As Boolean
 
     Private Sub Start_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
         Dim ListArray As String() = File.ReadAllLines(".\assets\test.txt")
         GuessWord = ListArray(Int(ListArray.Length * Rnd()))
         Console.WriteLine(GuessWord)
@@ -39,25 +41,37 @@ Public Class Start
 
         For index = 0 To 25
             If InputBox.Text = GuessedLetters(index) Then
-                MsgBox("|" & InputBox.Text & "| has already been used", 0, "Message")
-
+                MsgBox("| " & InputBox.Text & " | has already been used", 0, "Message")
                 Exit Sub
             End If
         Next
 
         For index = 0 To GuessWord.Length - 1
             If (InputBox.Text.Equals(WordArray(index), StringComparison.InvariantCultureIgnoreCase)) Then
-                ProgressArray(index) = WordArray(index)
+                Correct = True
+                Exit For
             Else
-                InputBox.Text = GuessedLetters(GuessedIndex)
-                GuessedIndex = GuessedIndex + 1
-                Lives = Lives - 1
+                Correct = False
+                Exit For
             End If
         Next
 
+        Console.WriteLine(Correct)
+        InputBox.Clear()
 
-            For index = 0 To WordArray.Length - 1
+
+        For index = 0 To WordArray.Length - 1
             GuessLetters.Text = Join(ProgressArray, " ")
         Next
     End Sub
+
+    Function LinearSearch(Target As String, SearchSpace() As String, Length As Integer) As Boolean
+        For index = 0 To Length
+            If Target = SearchSpace(index) Then
+                Return True
+            Else
+                Return False
+            End If
+        Next
+    End Function
 End Class
